a367dfcc0a95256d00ee64f419a29040b4eccf80
TavionK <tavion.k.britt99@gmail.com>
2025-06-20 11:04:01 -0400

    added `type` property to all blog post objects in `posts.js`

--- posts.js
+++ posts.js
@@ -1,93 +1,106 @@
 export const posts = [
     {
-        link: 'https://medium.com/@makayla.b.ferrell/tryhackme-w1seguy-726d45081a4c',
-        src: 'img/gandalf.png',
-        alt: 'Gandalf',
-        title: 'Prompt Hacking Gandalf AI',
-        desc: 'Use prompt injection to bypass Gandalf AI.'
+      link: 'https://medium.com/@makayla.b.ferrell/tryhackme-w1seguy-726d45081a4c',
+      src: 'img/gandalf.png',
+      alt: 'Gandalf',
+      title: 'Prompt Hacking Gandalf AI',
+      desc: 'Use prompt injection to bypass Gandalf AI.',
+      type: 'prompt'
     },
     {
-        link: 'https://medium.com/@makayla.b.ferrell/tryhackme-w1seguy-726d45081a4c',
-        src: 'img/xor2.png',
-        alt: 'W1seGuy',
-        title: 'Tryhackme W1seGuy',
-        desc: 'This room is a CTF style cryptography challenge that focuses on the XOR operator.'
+      link: 'https://medium.com/@makayla.b.ferrell/tryhackme-w1seguy-726d45081a4c',
+      src: 'img/xor2.png',
+      alt: 'W1seGuy',
+      title: 'Tryhackme W1seGuy',
+      desc: 'This room is a CTF style cryptography challenge that focuses on the XOR operator.',
+      type: 'CTF'
     },
     {
-        link: 'https://medium.com/@makayla.b.ferrell/tryhackme-traverse-f9959c945738',
-        src: '/img/api.png',
-        alt: 'Traverse',
-        title: 'Tryhackme Traverse',
-        desc: 'This room is a CTF style application security challenge that includes API hacking, python scripting, and command injection.'
+      link: 'https://medium.com/@makayla.b.ferrell/tryhackme-traverse-f9959c945738',
+      src: '/img/api.png',
+      alt: 'Traverse',
+      title: 'Tryhackme Traverse',
+      desc: 'This room is a CTF style application security challenge that includes API hacking, python scripting, and command injection.',
+      type: 'CTF'
     },
     {
-        link: 'https://medium.com/@makayla.b.ferrell/tryhackme-uranium-ba67a0697a21',
-        src: '/img/uranium.png',
-        alt: 'Uranium',
-        title: 'Tryhackme Uranium CTF',
-        desc: 'This room includes OSINT, phishing, wireshark forensics, and SUIDs.'
+      link: 'https://medium.com/@makayla.b.ferrell/tryhackme-uranium-ba67a0697a21',
+      src: '/img/uranium.png',
+      alt: 'Uranium',
+      title: 'Tryhackme Uranium CTF',
+      desc: 'This room includes OSINT, phishing, wireshark forensics, and SUIDs.',
+      type: 'CTF'
     },
     {
-        link: 'https://medium.com/@makayla.b.ferrell/tryhackme-brooklyn-nine-nine-3765baeb3f73',
-        src: '/img/b99.jpg',
-        alt: 'Brooklyn Nine Nine',
-        title: 'Tryhackme Brooklyn Nine Nine',
-        desc: 'This is a beginner room that includes anonymous ftp login, steganography, and linux privilege escalation.'
+      link: 'https://medium.com/@makayla.b.ferrell/tryhackme-brooklyn-nine-nine-3765baeb3f73',
+      src: '/img/b99.jpg',
+      alt: 'Brooklyn Nine Nine',
+      title: 'Tryhackme Brooklyn Nine Nine',
+      desc: 'This is a beginner room that includes anonymous ftp login, steganography, and linux privilege escalation.',
+      type: 'CTF'
     },
     {
-        link: 'https://medium.com/@makayla.b.ferrell/tryhackme-attacktive-directory-8a4eeee05ea4',
-        src: '/img/ad.jpg',
-        alt: 'TraAttacktive Directoryverse',
-        title: 'Tryhackme Attacktive Directory',
-        desc: 'This room covers exploiting an active directory domain controller.'
+      link: 'https://medium.com/@makayla.b.ferrell/tryhackme-attacktive-directory-8a4eeee05ea4',
+      src: '/img/ad.jpg',
+      alt: 'TraAttacktive Directoryverse',
+      title: 'Tryhackme Attacktive Directory',
+      desc: 'This room covers exploiting an active directory domain controller.',
+      type: 'CTF'
     },
     {
-        link: 'https://medium.com/@makayla.b.ferrell/tryhackme-looking-glass-767626f60858',
-        src: '/img/alice.jpg',
-        alt: 'Looking Glass',
-        title: 'Tryhackme Looking Glass',
-        desc: 'This room covers cryptography, linux enumeration, lateral and vertical privilege escalation.'
+      link: 'https://medium.com/@makayla.b.ferrell/tryhackme-looking-glass-767626f60858',
+      src: '/img/alice.jpg',
+      alt: 'Looking Glass',
+      title: 'Tryhackme Looking Glass',
+      desc: 'This room covers cryptography, linux enumeration, lateral and vertical privilege escalation.',
+      type: 'CTF'
     },
     {
-        link: 'https://medium.com/@makayla.b.ferrell/tryhackme-nerdherd-room-b6a8ec0bab05',
-        src: '/img/nerdherd.png',
-        alt: 'NerdHerd',
-        title: 'Tryhackme NerdHerd',
-        desc: 'This room is a ctf style challenge that covers ftp anonymous login, smb enumeration, vigenere ciphers, and a linux kernel privilege escalation.'
+      link: 'https://medium.com/@makayla.b.ferrell/tryhackme-nerdherd-room-b6a8ec0bab05',
+      src: '/img/nerdherd.png',
+      alt: 'NerdHerd',
+      title: 'Tryhackme NerdHerd',
+      desc: 'This room is a ctf style challenge that covers ftp anonymous login, smb enumeration, vigenere ciphers, and a linux kernel privilege escalation.',
+      type: 'CTF'
     },
     {
-        link: 'https://medium.com/@makayla.b.ferrell/tryhackme-git-happens-823dfe87cf0b',
-        src: '/img/git.png',
-        alt: 'Git Happens',
-        title: 'Tryhackme Git Happens',
-        desc: 'This room covers enumerating a public git repository.'
+      link: 'https://medium.com/@makayla.b.ferrell/tryhackme-git-happens-823dfe87cf0b',
+      src: '/img/git.png',
+      alt: 'Git Happens',
+      title: 'Tryhackme Git Happens',
+      desc: 'This room covers enumerating a public git repository.',
+      type: 'CTF'
     },
     {
-        link: 'https://medium.com/@makayla.b.ferrell/tryhackme-kenobi-room-46965d0d4501',
-        src: '/img/kenobi.png',
-        alt: 'Kenobi',
-        title: 'Tryhackme Kenobi',
-        desc: 'This room covers samba enumeration, ftp exploitation, and linux privilege escalation.'
+      link: 'https://medium.com/@makayla.b.ferrell/tryhackme-kenobi-room-46965d0d4501',
+      src: '/img/kenobi.png',
+      alt: 'Kenobi',
+      title: 'Tryhackme Kenobi',
+      desc: 'This room covers samba enumeration, ftp exploitation, and linux privilege escalation.',
+      type: 'CTF'
     },
     {
-        link: 'https://medium.com/@makayla.b.ferrell/tryhackme-wonderland-dafcc5a3efc',
-        src: '/img/wonder.png',
-        alt: 'Wonderland',
-        title: 'Tryhackme Wonderland',
-        desc: 'This room covers python library hijacking, linux enumeration, and privilege escalation.'
+      link: 'https://medium.com/@makayla.b.ferrell/tryhackme-wonderland-dafcc5a3efc',
+      src: '/img/wonder.png',
+      alt: 'Wonderland',
+      title: 'Tryhackme Wonderland',
+      desc: 'This room covers python library hijacking, linux enumeration, and privilege escalation.',
+      type: 'CTF'
     },
     {
-        link: 'https://medium.com/@makayla.b.ferrell/tryhackme-inclusion-room-d84cabfedf14',
-        src: 'img/inclus.png',
-        alt: 'Inclusion',
-        title: 'Tryhackme Inclusion',
-        desc: 'This room is for practice with Local File Inclusion (LFI) attacks.'
+      link: 'https://medium.com/@makayla.b.ferrell/tryhackme-inclusion-room-d84cabfedf14',
+      src: 'img/inclus.png',
+      alt: 'Inclusion',
+      title: 'Tryhackme Inclusion',
+      desc: 'This room is for practice with Local File Inclusion (LFI) attacks.',
+      type: 'CTF'
     },
     {
-        link: 'https://medium.com/@makayla.b.ferrell/tryhackme-easy-peasy-writeup-e688da86044b',
-        src: 'img/easyp.png',
-        alt: 'Easy Peasy',
-        title: 'Tryhackme Easy Peasy',
-        desc: 'This room is for practice locating a hidden directory to get initial access to a vulnerable machine, and escalating privileges using a crontab.'
+      link: 'https://medium.com/@makayla.b.ferrell/tryhackme-easy-peasy-writeup-e688da86044b',
+      src: 'img/easyp.png',
+      alt: 'Easy Peasy',
+      title: 'Tryhackme Easy Peasy',
+      desc: 'This room is for practice locating a hidden directory to get initial access to a vulnerable machine, and escalating privileges using a crontab.',
+      type: 'CTF'
     }
 ]
