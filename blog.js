20c8d1d8f87f853be02a1c0a05ed9f32b34a71fb
TavionK <tavion.k.britt99@gmail.com>
2025-06-19 21:21:43 -0400

    migrated to TailwindCSS with mobile-first responsive design and revamped HTML structure across all pages

--- blog.js
+++ blog.js
@@ -1,2 +1,29 @@
-// Optional for theme toggle or interactivity
-console.log('Blog page loaded');
+import { posts } from "./posts.js";
+const blogPostSection = document.getElementById("blog-post-section");
+const currentYear = new Date().getFullYear();
+
+function getBlogPostsHtml() {
+  return posts.map((post) => {
+    return `
+            <a href="${post.link}" class="flex flex-1">
+                <div class="group w-full bg-[#2a2e35] rounded-lg overflow-hidden transition duration-300 hover:-translate-y-1">
+                    <img class="w-full h-[200px] object-cover md:grayscale-55 transition duration-300 ease-in-out group-hover:grayscale-0" alt="${post.alt}" src="${post.src}" />
+                    <div class="px-4 pb-4">
+                        <h2 class="font-bold text-[var(--primary-color)] text-xl my-4">${post.title}</h2>
+                        <p>${post.desc}</p>
+                    </div>
+                </div>
+            </a>`;
+  });
+}
+
+function renderPosts() {
+  blogPostSection.innerHTML = getBlogPostsHtml().join("");
+}
+
+function renderCopyright() {
+  document.getElementById("footer").innerHTML =
+    `<p class="text-center text-[var(--gray-font)] my-4 text-xs md:text-base">Copyright &copy; ${currentYear} MakaylaDecoded. All rights reserved.</p>`;
+}
+renderCopyright();
+renderPosts();
