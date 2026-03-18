cdc6ce9b5a8782c4295d9ea8632b6934546ca6f3
TavionK <tavion.k.britt99@gmail.com>
2026-03-18 17:22:07 -0400

    added `posts.ts` with ACRTP and TryHackMe write-ups, redesigned home page sections with responsive hero banner, latest blogs, and added custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="flex flex-col items-center justify-center h-dvh">
+      <h1 className="text-primary font-bold text-6xl">404</h1>
+      <h2 className="text-gray-font">Page Not Found</h2>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,51 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <main>
+      {/*Hero Section*/}
+      <section className="container mx-auto mb-10 gap-6 max-sm:px-6 flex flex-col justify-center items-center md:flex-row md:justify-between">
+        <div className="md:w-1/2 text-center md:text-left">
+          <h1 className="text-4xl font-bold mb-8 leading-12">
+            Hi, I'm <span className="text-primary">Makayla Ferrell.</span>
+            <br />
+            This is my life <span className="text-primary">Decoded.</span>
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="text-gray-font">
+            I’m a software engineer turned ethical hacker who loves breaking
+            things (responsibly). This site is home to my write-ups, information
+            about me, and ways to get in touch. Click around and see what I’ve
+            been up to!
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="relative w-full h-100 md:w-1/2 md:flex md:justify-end">
+          <Image
+            className="rounded-lg md:size-4/5"
+            fill
+            src="/img/grad.JPG"
+            alt="Makayla Ferrell graduation photo"
+          />
         </div>
-      </main>
-    </div>
+      </section>
+      {/*Latest Blogs*/}
+      <section className="container mx-auto max-sm:px-6 mb-10">
+        <h2 className="text-center md:text-left text-xl md:text-2xl font-bold mb-4">
+          Latest Blog Posts
+        </h2>
+        <div
+          className="flex flex-col gap-4 md:flex-row"
+          id="featured-posts"
+        ></div>
+        <div className="flex justify-center my-4">
+          <Link
+            className="bg-primary hover:bg-secondary rounded-md px-2 py-1 duration-300 ease-in-out"
+            href="/blog.html"
+          >
+            View All
+          </Link>
+        </div>
+      </section>
+    </main>
   );
 }
