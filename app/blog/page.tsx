1736f635f6028349c44e87a088a5c31bd2a40635
TavionK <tavion.k.britt99@gmail.com>
2026-03-18 18:59:18 -0400

    added `Blog` page with a title and basic layout

---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <main>
+      <h1 className="text-4xl text-center font-bold">My <span className="text-primary">Blog</span></h1>
+    </main>
+  )
+}
