f3650b8db41896cae76a998d720d7aea2dc2ea83
TavionK <tavion.k.britt99@gmail.com>
2026-03-18 19:19:11 -0400

    added `Contact` page with title and basic layout

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="px-4 py-10 flex-1">
+      <h1 className="text-4xl text-center font-bold">
+        Contact <span className="text-primary">Me</span>
+      </h1>
+    </main>
+  );
+}
