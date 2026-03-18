8439aaf005ad1a7fbe6bd320fc0670ae032e93f2
TavionK <tavion.k.britt99@gmail.com>
2026-03-18 19:13:52 -0400

    refined `NotFound` page layout for better vertical alignment and updated heading font weight

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
-    <main className="flex flex-col items-center justify-center h-dvh">
-      <h1 className="text-primary font-bold text-6xl">404</h1>
+    <main className="flex flex-col items-center justify-center flex-1">
+      <h1 className="text-primary font-semibold text-6xl">404</h1>
       <h2 className="text-gray-font">Page Not Found</h2>
     </main>
   );
